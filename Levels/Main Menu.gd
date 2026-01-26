58a2a0a72edf1303c22b58459781d226bc08dc99
kirchemz <kirchemz@gmail.com>
2026-01-25 19:01:31 -0500

    Update Main Menu.gd

--- Levels/Main Menu.gd
+++ Levels/Main Menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Levels/level_four.tscn")
+	get_tree().change_scene_to_file("res://Levels/level_one.tscn")
 
 
 func _on_credits_pressed():
